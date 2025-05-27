c826f553be45e44d331613b79f4570d323f51269
watai <watai@DESKTOP-FMU8U8S.mshome.net>
2025-05-27 14:55:28 +0900

    テーブル登録の再修正

--- src/main/java/com/example/fridgemanager/entity/User.java
+++ src/main/java/com/example/fridgemanager/entity/User.java
@@ -35,7 +35,7 @@ public class User {
     private String password;
 
     
-    @ManyToMany
+    @ManyToMany(mappedBy = "users")
     private List<Fridge> fridges = new ArrayList<>();
 
     // --- コンストラクタ ---
