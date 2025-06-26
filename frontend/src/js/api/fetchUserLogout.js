fc2018cbfaac8a3746ac95d3a82d443eafdd8831
FUJIKURAH <haru.qqq0223@gmail.com>
2025-06-26 22:13:38 +0900

    Update fetchUserLogout.js

--- frontend/src/js/api/fetchUserLogout.js
+++ frontend/src/js/api/fetchUserLogout.js
@@ -7,9 +7,6 @@ async function fetchUserLogout() {
     try {
         const res = await fetch(`/api/logout`, {
           method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
           credentials: 'include'
         });
     
