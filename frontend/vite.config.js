7b8ea009d7b43c50cba950f07674a62ce6ac1f93
FUJIKURAH <haru.qqq0223@gmail.com>
2025-07-15 22:29:44 +0900

    index create

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     outDir: 'dist',    // ビルド出力先
     rollupOptions: {
       input: {
+        main: resolve(__dirname, 'index.html'),
         register: resolve(__dirname, 'register.html'),
         login: resolve(__dirname, 'login.html'),
         fridges: resolve(__dirname, 'fridges.html'),
