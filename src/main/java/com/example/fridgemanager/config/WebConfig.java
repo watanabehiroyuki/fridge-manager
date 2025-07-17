9971d1d5ec4e60779c900feaeb1a4132632a31b3
FUJIKURAH <haru.qqq0223@gmail.com>
2025-07-17 09:58:02 +0900

    Update WebConfig.java

--- src/main/java/com/example/fridgemanager/config/WebConfig.java
+++ src/main/java/com/example/fridgemanager/config/WebConfig.java
@@ -9,7 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                    "http://localhost:5173",
+                    "https://fridge-manager-portfolio.netlify.app" 
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
