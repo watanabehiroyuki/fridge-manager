2a443cea369ca3a635c603e40cdb8ae27ad6fc66
FUJIKURAH <haru.qqq0223@gmail.com>
2025-07-29 11:36:31 +0900

    Update FridgeController.java

--- src/main/java/com/example/fridgemanager/controller/FridgeController.java
+++ src/main/java/com/example/fridgemanager/controller/FridgeController.java
@@ -131,16 +131,20 @@ public class FridgeController {
     }
     
     @GetMapping("/with-details")
-        public List<FridgeDetailDTO> getFridgeDetails(Principal principal) {
-            System.out.println("DEBUG: /with-details called");
-            if (principal == null) {
-                System.out.println("DEBUG: principal is NULL!");
-                throw new RuntimeException("Principal is null");
-            }
-            System.out.println("DEBUG: principal = " + principal.getName());
-
-            return fridgeService.getFridgeDetailsByUserEmail(principal.getName());
+    public List<FridgeDetailDTO> getFridgeDetails(Principal principal) {
+        return fridgeService.getFridgeDetailsByUserEmail(principal.getName());
     }
 
+    //     @GetMapping("/with-details")
+    //     public List<FridgeDetailDTO> getFridgeDetails(Principal principal) {
+    //         System.out.println("DEBUG: /with-details called");
+    //         if (principal == null) {
+    //             System.out.println("DEBUG: principal is NULL!");
+    //             throw new RuntimeException("Principal is null");
+    //         }
+    //         System.out.println("DEBUG: principal = " + principal.getName());
+
+    //         return fridgeService.getFridgeDetailsByUserEmail(principal.getName());
+    // }
     
 }
