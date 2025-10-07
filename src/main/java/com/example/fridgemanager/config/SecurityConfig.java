6d0885d1c7f0d3936a80a78b6f3c237539a15a48
FUJIKURAH <haru.qqq0223@gmail.com>
2025-10-07 15:38:14 +0900

    Update SecurityConfig.java

--- src/main/java/com/example/fridgemanager/config/SecurityConfig.java
+++ src/main/java/com/example/fridgemanager/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         return http
             .csrf().disable()
             .authorizeHttpRequests(authz -> authz
-            		.antMatchers("/api/register","register-sucess", "/api/login","/api/notify/send","/api/logout").permitAll() // 認証不要なURL
+            		.antMatchers("/login","/register","/api/register","/register-sucess", "/api/login","/api/notify/send","/api/logout").permitAll() // 認証不要なURL
                 .anyRequest().authenticated() // その他は認証が必要
             )
             .formLogin().disable() // ← これ重要！フォームログインは無効
@@ -54,19 +54,19 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
     }
+w
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("http://localhost:5500")); // ← HTMLの表示元に合わせる
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowCredentials(true); // Cookieなどが必要な場合
 
-    // @Bean
-    // public CorsConfigurationSource corsConfigurationSource() {
-    // CorsConfiguration configuration = new CorsConfiguration();
-    // configuration.setAllowedOrigins(List.of("http://localhost:5500")); // ← HTMLの表示元に合わせる
-    // configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-    // configuration.setAllowedHeaders(List.of("*"));
-    // configuration.setAllowCredentials(true); // Cookieなどが必要な場合
-
-    // UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    // source.registerCorsConfiguration("/**", configuration);
-    // return source;
-    // }
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+    }
 }
 
 
