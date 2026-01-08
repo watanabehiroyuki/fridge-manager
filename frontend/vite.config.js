e0afc9be733e5ef6933c47a613d1cc4d7cb85fb7
FUJIKURAH <haru.qqq0223@gmail.com>
2026-01-09 00:13:40 +0900

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,31 +1,30 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  root: '.',           // プロジェクトルート（必要なら変更）
-  publicDir: 'public', // 静的ファイル用
+  root: ".", // プロジェクトルート（必要なら変更）
+  publicDir: "public", // 静的ファイル用
   build: {
-    outDir: 'dist',    // ビルド出力先
+    outDir: "dist", // ビルド出力先
     rollupOptions: {
       input: {
-        register: resolve(__dirname, 'register.html'),
-        login: resolve(__dirname, 'login.html'),
-        fridges: resolve(__dirname, 'fridges.html'),
-        fridgeDetail: resolve(__dirname, 'fridgeDetail.html'),
-        userShare: resolve(__dirname, 'userShare.html')
+        register: resolve(__dirname, "register.html"),
+        login: resolve(__dirname, "login.html"),
+        fridges: resolve(__dirname, "fridges.html"),
+        fridgeDetail: resolve(__dirname, "fridgeDetail.html"),
+        userShare: resolve(__dirname, "userShare.html"),
       },
     },
   },
   server: {
-    port: 5173,        // 開発サーバーのポート（任意）
+    host: true,
+    port: 5173, // 開発サーバーのポート（任意）
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
+      "/api": {
+        target: "http://localhost:8080",
         changeOrigin: true,
-        secure: false
-      }
-    }
+        secure: false,
+      },
+    },
   },
 });
-
-
