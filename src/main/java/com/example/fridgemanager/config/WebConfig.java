45ccedce44ff9e70cbdcd92c5862bc451e9b824b
FUJIKURAH <haru.qqq0223@gmail.com>
2026-01-12 15:37:27 +0900

    Update WebConfig.java

--- src/main/java/com/example/fridgemanager/config/WebConfig.java
+++ src/main/java/com/example/fridgemanager/config/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
                     "http://10.*.*.*:5173",
                     "http://172.16.*.*:5173",
                     "https://fridge-manager-portfolio.netlify.app" ,
-                    "https://fridge-manager-portfolio.netlify-v2.app" ,
+                    "https://fridge-manager-v2.netlify.app" ,
                     "http://35.72.189.212"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
